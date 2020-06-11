4bd5b5a6fbfbc411be524bde6d71d2e2651ddee2
bassel ahmed alazab <bassel.alazab@gmail.com>
2020-06-11 17:03:38 +0200

    Create Members  Data

---
+++ src/infoClasses/Members.java
@@ -0,0 +1,19 @@
+
+package infoClasses;
+public class Members {
+    
+    private int membId;
+    private String fName;
+    private String mName;
+    private String lName;
+    private String email;
+    private int phone;
+    private String address;
+    private int numBookBorrowed;
+    private int numBookBought;
+    
+    public Members(int newMembId, String newFName, String newMName, String newLName, String newEmail,
+                   int newPhone, String newAddress, int newNumBookBorrowed,int newNumBookBought){
+        
+    }
+}
