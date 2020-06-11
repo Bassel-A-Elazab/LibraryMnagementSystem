1798b66b32ab08a5f6ae8c888e09f25469aeeba1
bassel ahmed alazab <bassel.alazab@gmail.com>
2020-06-11 19:00:27 +0200

    Fill Books Category

--- src/infoClasses/Category.java
+++ src/infoClasses/Category.java
@@ -1,4 +1,13 @@
 package infoClasses;
 public class Category {
-    
+    private String name;
+    public Category(String newName){
+        this.name = name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public String getName(){
+        return name;
+    }
 }
