34ca873b016f69c60c9326b2a112d41fd8aca9e1
bassel ahmed alazab <bassel.alazab@gmail.com>
2020-07-01 01:14:33 +0200

    Add Set&Get Method To Borrowed Or Bought Books For A Member

--- src/infoClasses/Members.java
+++ src/infoClasses/Members.java
@@ -1,12 +1,34 @@
-
 package infoClasses;
-public class Members extends MainPersonInfo{
+
+public class Members extends MainPersonInfo {
+
     private int numBookBorrowed;
     private int numBookBought;
 
     public Members(String newFName, String newMName, String newLName,
-                   String newEmail, String newPhone, String newAddress) {
+            String newEmail, String newPhone, String newAddress) {
         super(newFName, newMName, newLName, newEmail, newPhone, newAddress);
     }
-    
+
+    public Members(String newFName, String newMName, String newLName,
+            String newEmail, String newPhone, String newAddress,
+            int newNumBookBorrowed, int newNumBookBought) {
+        super(newFName, newMName, newLName, newEmail, newPhone, newAddress);
+    }
+
+    public void setBookBorrowed(int newNumBookBorrowed) {
+        this.numBookBorrowed = newNumBookBorrowed;
+    }
+
+    public int getBookBorrowed() {
+        return numBookBorrowed;
+    }
+
+    public void setBookBought(int newNumBookBought) {
+        this.numBookBought = newNumBookBought;
+    }
+
+    public int getBookBought() {
+        return numBookBought;
+    }
 }
